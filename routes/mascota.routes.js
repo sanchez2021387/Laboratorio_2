7a06622ff94cb63b00cd279dcf1098b0c5b4acb6
sanchez2021387 <jsanchez2021387@gmail.com>
2024-02-23 08:13:51 -0600

    Endpoint mascotaPost posee una validación solucionado el error

---
+++ routes/mascota.routes.js
@@ -0,0 +1,49 @@
+const {Router} = require('express');
+const {check} = require('express-validator');
+
+const { validarCampos } = require('../middlewares/validar-campos');
+const { existenteEmail, existeMascotaById,} = require('../helpers/db-validators');
+
+const {   mascotaDelete,mascotaPost,mascotaPut,getMascotaById, mascotaGet,
+} = require('../controllers/mascota.controller');
+
+const router = Router();
+
+router.get("/", mascotaGet);
+
+router.get(
+    "/:id",
+    [
+        check("id","El id no es un formato válido de MongoDB").isMongoId(),
+        check("id").custom(existeMascotaById),
+        validarCampos
+    ], getMascotaById);
+
+router.put(
+    "/:id",
+    [
+        check("id","El id no es un formato válido de MongoDB").isMongoId(),
+        check("id").custom(existeMascotaById),
+        validarCampos
+    ], mascotaPut);
+
+router.delete(
+    "/:id",
+        [
+        check("id","El id no es un formato válido de MongoDB").isMongoId(),
+        check("id").custom(existeMascotaById),            validarCampos
+     ], mascotaDelete);
+
+router.post(
+    "/",
+    [
+        check("nombre", "El nombre es obligatorio").not().isEmpty(),
+        check("raza", "La raza es obligatoria"),
+        check("edad","La edad es obligatoria").isLength(),
+        check("sexo", "el sexo debe ser obligatorio"),
+        validarCampos,
+    ],mascotaPost);
+
+    module.exports =router;
+
+
